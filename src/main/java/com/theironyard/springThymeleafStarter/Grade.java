0222a9ab294f8c0359b29c1a86c6807e9dd8cde4
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-09-27 08:31:22 -0500

    create new student form

--- src/main/java/com/theironyard/springThymeleafStarter/Grade.java
+++ src/main/java/com/theironyard/springThymeleafStarter/Grade.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.theironyard.springThymeleafStarter;
 
 public enum Grade {
     First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eighth, Ninth, Tenth, Eleventh, Twelfth
