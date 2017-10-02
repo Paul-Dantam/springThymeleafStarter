5f565cf8647f5a61c8d96d52009ef4d11cce3185
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-10-02 08:41:18 -0500

    create redirect, fix html semantics, remove unnecessary Model model calls

--- src/main/java/com/theironyard/springThymeleafStarter/StudentController.java
+++ src/main/java/com/theironyard/springThymeleafStarter/StudentController.java
@@ -5,34 +5,39 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class StudentController {
 
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String index(Model model)
-    {
+    public String index() {
         return "index";
     }
 
     @RequestMapping(path = "/new_student", method = RequestMethod.GET)
     public String newStudent(Model model) {
-        model.addAttribute("grades", Grade.values() );
+        model.addAttribute("grades", Grade.values());
         return "new_student";
     }
 
     @RequestMapping(path = "/create_student", method = RequestMethod.POST)
-    public String createStudent(@RequestParam(value = "firstName") String firstName,
-                                @RequestParam(value = "lastName") String lastName,
-                                @RequestParam(value = "grade") Grade grade, Model model)
-    {
+    public String createStudent(@RequestParam(value = "first_name") String firstName,
+                                @RequestParam(value = "last_name") String lastName,
+                                @RequestParam(value = "grade") Grade grade,
+                                RedirectAttributes redirectAttributes) {
         Student student = new Student();
-        student.setFirstName( firstName );
-        student.setLastName( lastName );
-        student.setGrade( grade );
-        model.addAttribute( "student", student );
-        return "view_student";
+        student.setFirstName(firstName);
+        student.setLastName(lastName);
+        student.setGrade(grade);
+        redirectAttributes.addFlashAttribute(student);
+        return "redirect:/view_student";
+    }
+
+    @RequestMapping(path = "/view_student", method = RequestMethod.GET)
+    public String viewStudent(Model model) {
+        return "/view_student";
     }
 
 }
